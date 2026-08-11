42ae3bbdc90f9d98b79636e3c8064d26afec578b
KatieK164 <katiekeegan3@gmail.com>
2026-08-11 16:31:03 +0100

    Initial commit

---
+++ shot-service/src/main/java/com/micropool/shotservice/ShotServiceApplication.java
@@ -0,0 +1,12 @@
+package com.micropool.shotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShotServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShotServiceApplication.class, args);
+    }
+}
