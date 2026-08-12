8de55f26170600ff14ddce7bd487a4986708c824
Tadhg Donnelly <tadhg.donnelly@ericsson.com>
2026-08-12 10:54:04 +0100

    comment

--- match-service/src/main/java/com/micropool/matchservice/model/ShotResult.java
+++ match-service/src/main/java/com/micropool/matchservice/model/ShotResult.java
@@ -4,6 +4,7 @@ package com.micropool.matchservice.model;
 public enum ShotResult {
     FOUL,
     MISS,
+    // design simplification -- it's very rare that you will pot more than 2 balls on the one shot
     POT_ONE,
     POT_TWO
 }
